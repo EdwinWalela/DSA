325b031e6d27aad22cbdfb1ad41b5ac382fdca68
edwinwalela <edwinwalela@gmail.com>
2023-11-02 09:35:49 +0300

    number of islands

--- graphs/number-of-islands.py
+++ graphs/number-of-islands.py
@@ -6,37 +6,37 @@ def numIslands(grid:List[List[str]])->int:
   if not grid:
     return 0
   
-  # Get grid dimensions
-  rows, cols = len(grid), len(grid[0])
-  visit = set()
+  rows, cols = len(grid), len(grid[0])  
+  visited = set()
   islands = 0
   
   def bfs(r,c):
+    # Doubly Ended queue
+    # Quicker append and pop from both ends 
+    # complexity => O(1)
+    # list append and pop  => O(n)
     q = collections.deque()
-    visit.add((r,c))
+    visited.add((r,c))
     q.append((r,c))
     
+    # while queue is not empty
     while q:
-      row,col = q.popleft()
+      row, col = q.popleft()
       directions = [[1,0],[-1,0],[0,1],[0,-1]]
-      
-      # Check adjacent nodes (top,bottom,right,left)
-      for dr,dc in directions:
-        r = row + dr
-        c = col + dc        
-       
-        if(
-            r in range(rows) and  # Check if row is within bounds of our grid
-            c in range(cols) and  # Check if col is within bounds of our grid
-            grid[r][c] == "1" and # Check if current value is land
-            (r ,c) not in visit   # Check if current value hasn't been visited
-          ):
+      for dr, dc in directions:
+        r, c = row + dr, col + dc
+        if (
+          r in range(rows) and
+          c in range(cols) and
+          grid[r][c] == "1" and
+          (r, c) not in visited
+        ):
           q.append((r,c))
-          visit.add((r,c))
-   
+          visited.add((r,c))
+  
   for r in range(rows):
     for c in range(cols):
-      if grid[r][c] == "1" and (r,c) not in visit:
+      if grid[r][c] == "1" and (r,c) not in visited:
         bfs(r,c)
         islands += 1
   return islands
@@ -44,7 +44,7 @@ def numIslands(grid:List[List[str]])->int:
 
 
 grid = [
- ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
   ["1","1","0","0","0"],
   ["0","0","1","0","0"],
   ["0","0","0","1","1"]
